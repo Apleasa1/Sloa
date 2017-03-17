e4846d4d5e21f8c3626dd10c930c21ac2dfe0d87
Apleasa1 <Apleasa1@dtcc.edu>
2017-03-17 13:08:42 -0400

    Add files via upload

---
+++ sloa project/xyz.js
@@ -0,0 +1,19 @@
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { /* When it reaches last image */
+		slideIndex = 1
+	} 
+	
+    x[slideIndex-1].style.display = "block"; 
+	
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
